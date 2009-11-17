5a513034c7e877299440203bc36e26d5593eaf16
knabar <knabar@b759562e-c88f-11de-a9c8-2fbcfba63733>
2009-11-17 17:13:04 +0000

    Added SSL flag on login URL

--- trunk/jmutube/urls.py
+++ trunk/jmutube/urls.py
@@ -10,7 +10,7 @@ from views import media_main, migrate_files, media_delete, media_rename, thumbna
 urlpatterns = patterns('',
     (r'^$', direct_to_template, {'template': 'jmutube-home.html'}),
 
-    url(r'^admin/(.*)', admin.site.root, name='admin'),
+    url(r'^admin/(.*)', admin.site.root, {'SSL': True}, name='admin'),
     url(r'^mstatic/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_DIR}, name='jmutube-master-static'),
 
     url(r'^accounts/login/$', login, {'template_name': 'registration/jmutube-login.html', 'SSL': True}, name='jmutube-login'),
