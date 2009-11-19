30a6d62fe894b0ec8109e4eabf27423e65287296
knabar <knabar@b759562e-c88f-11de-a9c8-2fbcfba63733>
2009-11-19 15:09:27 +0000

    Updated JMUtube local settings template

--- jmutube/settings_local.template.py
+++ jmutube/settings_local.template.py
@@ -1,3 +1,10 @@
+
+#ROOT_URLCONF = 'apps.jmutube.urls'
+#JMUTUBE_LOGIN_URL = '/accounts/login/'
+
+JMUTUBE_LOGIN_URL = '/jmutube/accounts/login/'
+
+
 # Path information
 
 JMUTUBE_STATIC_FILES          = 'd:/dev/rooibos/apps/jmutube/static'
@@ -5,7 +12,6 @@ JMUTUBE_MEDIA_ROOT            = 'd:/dev/jmutube/media'
 JMUTUBE_CRASS_MISC_FOLDER     = 'd:/dev/jmutube/media/misc'
 JMUTUBE_RELAY_INCOMING_FOLDER = 'd:/dev/jmutube/media/relay_incoming'
 
-JMUTUBE_LOGIN_URL = '/jmutube/accounts/login/'
 
 # Don't need to change anything below this line
 
@@ -21,3 +27,12 @@ INSTALLED_APPS = (
 STORAGE_SYSTEMS = {
     'jmutube': 'apps.jmutube.jmutubestorage.JMUtubeStorageSystem',
 }
+
+
+LOGIN_CHECKS = (
+    'apps.jmutube.settings_local.verify_user',
+)
+
+
+def verify_user(user, attributes):
+    return any(r in ('faculty', 'staff', 'administrator') for r in attributes.get('eduPersonPrimaryAffiliation', []))
